b59d36c961fb260a75df4f423fc8a9feb28fbe24
samanthaduarte <samanthaduarte>
2017-06-02 20:35:19 -0600

    Leer texto

---
+++ src/Texto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Texto {
+
+	public String leerTexto() throws IOException {
+		// TODO Auto-generated constructor stub
+		// Para leer del archivo de texto
+		
+		File file = new File("guategrafo.txt");
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		StringBuffer stringBuffer = new StringBuffer();
+		String line;
+		
+		while ((line = bufferedReader.readLine()) != null) {
+			stringBuffer.append(line);
+			stringBuffer.append(",");
+		}
+		fileReader.close();
+		
+		return stringBuffer.toString();
+	}
+}
+
